56cfa782d182b07380a4a3e0f92931e644674b37
shuimu98 <shui_mu98@163.com>
2017-11-08 19:20:06 +0800

    -maev

--- src/c-src/aev/ae_epoll.c
+++ src/c-src/aev/ae_epoll.c
@@ -42,8 +42,7 @@ static int aeApiAddEvent(aeEventLoop *eventLoop, int fd, int mask) {
     struct epoll_event ee;
     /* If the fd was already monitored for some event, we need a MOD
     * operation. Otherwise we need an ADD operation. */
-    int op = eventLoop->events[fd].mask == AE_NONE ?
-EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+    int op = eventLoop->events[fd].mask == AE_NONE ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
 
     ee.events = 0;
     mask |= eventLoop->events[fd].mask; /* Merge old events */
